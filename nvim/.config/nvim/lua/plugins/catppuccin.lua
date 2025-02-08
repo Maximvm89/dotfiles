0744769f7264ae4392d0c4dc5af758a08f714f2b
Marco Parisi <mparisi@sys-vm0065.ldn.framestore.com>
2025-02-08 13:53:49 +0000

    adding catpuccin and yazi

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,33 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha", -- Options: latte, frappe, macchiato, mocha
+      integrations = {
+        treesitter = true,
+        telescope = true,
+        nvimtree = true,
+        lsp_saga = true,
+        mason = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+      },
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
+}
