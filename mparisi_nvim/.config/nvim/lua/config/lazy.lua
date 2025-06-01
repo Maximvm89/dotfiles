4f0385fc22ec9ac48554e31c93338d0ca32aedf7
Marco Parisi <marcoparisi@Marcos-MacBook-Pro.local>
2025-06-02 00:08:19 +0100

    add

--- mparisi_nvim/.config/nvim/lua/config/lazy.lua
+++ mparisi_nvim/.config/nvim/lua/config/lazy.lua
@@ -1,16 +1,18 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if vim.fn.isdirectory(lazypath) == 0 then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    -- os.exit(1)
+if _G.PL_DEV then
+  if vim.fn.isdirectory(lazypath) == 0 then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      -- os.exit(1)
+    end
   end
 end
 vim.opt.rtp:prepend(lazypath)
