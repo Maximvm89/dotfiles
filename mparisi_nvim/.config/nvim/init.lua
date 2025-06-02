d0c8f9ba12dbe7680c8bce8d582ac7a34d3dd321
Marco Parisi <mparisi@sys-vm0065.ldn.framestore.com>
2025-06-02 11:32:32 +0100

    add local repos

--- mparisi_nvim/.config/nvim/init.lua
+++ mparisi_nvim/.config/nvim/init.lua
@@ -1,18 +1,20 @@
 -- init.lua
 if os.getenv("PL_ZONE") == "vfx" then
-  _G.PL_DEV = true
+	_G.PL_DEV = true
 else
-  _G.PL_DEV = false
+	_G.PL_DEV = false
 end
 
 if os.getenv("PL_SITE") then
-  _G.PL_SITE = true
+	_G.PL_SITE = true
 end
 
 if _G.PL_SITE then
-	 _G.Work_dir = "/net/code/workspaces/mparisi/git"
+	_G.Work_dir = "/net/code/workspaces/mparisi/git"
+	_G.Local_repos = "/net/code/workspaces/mparisi/lazyvim"
 else
-	 _G.Work_dir = "/Volumes/Framestore/git"
+	_G.Work_dir = "/Volumes/Framestore/git"
+	_G.Local_repos = ""
 end
 
 _G.Config_dir = "~/.config/nvim"
