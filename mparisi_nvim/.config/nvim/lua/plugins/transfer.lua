872771d3611728c143eb9d38b1eda9e52c7cfed6
Marco Parisi <mparisi@sys-vm0065.ldn.framestore.com>
2025-06-02 11:20:10 +0100

    cleaning

--- mparisi_nvim/.config/nvim/lua/plugins/transfer.lua
+++ mparisi_nvim/.config/nvim/lua/plugins/transfer.lua
@@ -1,7 +1,7 @@
 return {
 	"coffebar/transfer.nvim",
-	enabled = true,
-	dev = _G.PL_DEV,
+	enabled = not PL_SITE,
+	dev = PL_DEV,
 	lazy = true,
 	cmd = { "TransferInit", "DiffRemote", "TransferUpload", "TransferDownload", "TransferDirDiff", "TransferRepeat" },
 	opts = {},
