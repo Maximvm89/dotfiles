1ebc94ff16f1ab750aaf15cd65cac568814f202f
Marco Parisi <marcoparisi@Marcos-MacBook-Pro.local>
2025-06-02 23:18:10 +0100

    test

--- mparisi_nvim/.config/nvim/init.lua
+++ mparisi_nvim/.config/nvim/init.lua
@@ -1,12 +1,43 @@
 -- init.lua
+local logo_vfx = [[
+
+██╗░░░██╗███████╗██╗░░██╗
+██║░░░██║██╔════╝╚██╗██╔╝
+╚██╗░██╔╝█████╗░░░╚███╔╝░
+░╚████╔╝░██╔══╝░░░██╔██╗░
+░░╚██╔╝░░██║░░░░░██╔╝╚██╗
+░░░╚═╝░░░╚═╝░░░░░╚═╝░░╚═╝]]
+
+local logo_shared = [[
+	
+░██████╗██╗░░██╗░█████╗░██████╗░███████╗██████╗░
+██╔════╝██║░░██║██╔══██╗██╔══██╗██╔════╝██╔══██╗
+╚█████╗░███████║███████║██████╔╝█████╗░░██║░░██║
+░╚═══██╗██╔══██║██╔══██║██╔══██╗██╔══╝░░██║░░██║
+██████╔╝██║░░██║██║░░██║██║░░██║███████╗██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═════╝░]]
+
+local logo_local = [[
+	
+███╗░░░███╗░█████╗░░█████╗░░░░░░░░█████╗░░██████╗
+████╗░████║██╔══██╗██╔══██╗░░░░░░██╔══██╗██╔════╝
+██╔████╔██║███████║██║░░╚═╝█████╗██║░░██║╚█████╗░
+██║╚██╔╝██║██╔══██║██║░░██╗╚════╝██║░░██║░╚═══██╗
+██║░╚═╝░██║██║░░██║╚█████╔╝░░░░░░╚█████╔╝██████╔╝
+╚═╝░░░░░╚═╝╚═╝░░╚═╝░╚════╝░░░░░░░░╚════╝░╚═════╝░]]
+
+
 if os.getenv("PL_ZONE") == "vfx" then
 	_G.PL_DEV = true
 	_G.Theme = "nord"
+	_G.Logo = logo_vfx
 elseif os.getenv("PL_ZONE") == "shared" then
 	_G.Theme = "kanagawa"
+	_G.Logo = logo_shared
 else
 	-- _G.Theme = "catppuccin-frappe"
 	_G.Theme = "nord"
+	_G.Logo = logo_local
 	_G.PL_DEV = false
 end
 
@@ -14,7 +45,7 @@ if os.getenv("PL_SITE") then
 	_G.PL_SITE = true
 end
 
-if _G.PL_SITE then
+if _G.PL_SITE then  
 	_G.Work_dir = "/net/code/workspaces/mparisi/git"
 	_G.Local_repos = "/net/code/workspaces/mparisi/lazyvim"
 	_G.Treesitter_parsers_path = "/net/code/workspaces/mparisi/lazyvim/nvim-treesitter/parser"
