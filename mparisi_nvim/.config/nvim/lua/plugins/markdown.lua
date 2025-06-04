3599870fac3ce28112d3de17c8f608842b3d3f24
Marco Parisi <marcoparisi@Marcos-MacBook-Pro.local>
2025-06-04 11:45:27 +0100

    add markdown

--- mparisi_nvim/.config/nvim/lua/plugins/markdown.lua
+++ mparisi_nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,21 @@
 return {
 	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dev = PL_DEV,
+		dependencies = {
+			{ "nvim-treesitter/nvim-treesitter", dev = PL_DEV },
+			{ "echasnovski/mini.nvim", dev = PL_DEV },
+		}, -- if you use the mini.nvim suite
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+		---@module 'render-markdown'
+		---@type render.md.UserConfig
+		opts = {},
+	},
+	{
 		"tadmccorkle/markdown.nvim",
-		dev = _G.PL_DEV,
+		enabled = false,
+		dev = PL_DEV,
 		ft = "markdown", -- or 'event = "VeryLazy"'
 		opts = {
 			-- configuration here or empty for defaults
