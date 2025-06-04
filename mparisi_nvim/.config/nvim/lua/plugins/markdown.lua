8c31a77db7f0b8a375f988bc71917031c77aa870
marco <marco.parisi@framestore.com>
2025-06-04 06:32:28 +0000

    markdown and switch to kitty on arch

---
+++ mparisi_nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"tadmccorkle/markdown.nvim",
+		dev = _G.PL_DEV,
+		ft = "markdown", -- or 'event = "VeryLazy"'
+		opts = {
+			-- configuration here or empty for defaults
+		},
+	},
+}
