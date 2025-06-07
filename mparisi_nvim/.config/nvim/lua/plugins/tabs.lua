41b4c266bfad16b4cedaefc8defb58b203f3db59
Marco Parisi <mparisi@sys-vm0065.ldn.framestore.com>
2025-06-07 21:01:28 +0100

    fixing for svfx

--- mparisi_nvim/.config/nvim/lua/plugins/tabs.lua
+++ mparisi_nvim/.config/nvim/lua/plugins/tabs.lua
@@ -1,9 +1,10 @@
 return {
 	"willothy/nvim-cokeline",
+	dev = PL_DEV,
 	dependencies = {
-		"nvim-lua/plenary.nvim", -- Required for v0.4.0+
-		"nvim-tree/nvim-web-devicons", -- If you want devicons
-		"stevearc/resession.nvim", -- Optional, for persistent history
+		{"nvim-lua/plenary.nvim", dev = PL_DEV }, -- Required for v0.4.0+
+		{"nvim-tree/nvim-web-devicons", dev = PL_DEV }, -- If you want devicons
+		{"stevearc/resession.nvim", dev = PL_DEV }, -- Optional, for persistent history
 	},
 	config = true,
 }
